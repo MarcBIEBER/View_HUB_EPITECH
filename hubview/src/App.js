c7f9bcb76e0222fca54cf33d1219756aecf0150d
Marc <marc.bieber@epitech.eu>
2023-03-21 14:54:59 +0100

    [+] Adding auth path ready to secure all routes

--- hubview/src/App.js
+++ hubview/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes , Route } from 'react-router-dom';
 
 import Navbar from './layouts/navbar';
 import Login from './layouts/login';
+import Register from './layouts/register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
